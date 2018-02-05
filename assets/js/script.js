b775e3ff09c96f562bd8c60d32b8a4f8f3b29e84
Alexsandrrh <sadovspbsasha@outlook.com>
2018-02-05 18:58:10 +0300

    new js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,76 @@
 $(function() {
-    // Function
-    function showBubble(text) {
-    	$('.bubble').text(text);
-		$('.bubble').addClass('-visible');
-		setTimeout(function() {
-    	$('.bubble').removeClass('-visible');
-    }, 2000);
-    }
 
-    function showFristInput() {
-		var inputText = $('.js-firstNumber').val();
-		if (inputText == "") {
-			inputText = "NO TEXT";
-			showBubble("NO TEXT");
+    // PLUS
+	$('.js-plus').click(function() {
+		if (checkInputData()) {
+			showBubble(Number(getFirstWord()) + Number(getSecondWord()));
 		}
-		else {
-			showBubble(inputText);
-		}   	
-    }
-    
-    // Command
-	$('.js-count').click(function() {
-		showBubble($('.js-firstNumber').val() + $('.js-secondNumber').val());
 	});
 
+	// MINUS
+	$('.js-minus').click(function() {
+		if (checkInputData()) {
+			showBubble(Number(getFirstWord()) - Number(getSecondWord()));
+		}
+	});
+
+    // MULTIPLY
+	$('.js-multiply').click(function() {
+		if (checkInputData()) {
+			showBubble(Number(getFirstWord()) * Number(getSecondWord()));
+		}
+	});
+
+    // DIVIDE
+	$('.js-divide').click(function() {
+		if (checkInputData().isNumber) {
+			showBubble(Number(getFirstWord()) / Number(getSecondWord()));
+		} else {
+			if (checkInputData().empty) {
+				showBubble('NO');
+			} else {
+				showBubble(getFirstWord() + ' ' + getSecondWord());
+			}
+		}
+	});		
+
+
+    // ==============================================================
+    // ==============================================================
+
+    // FUNCTION
+    function showBubble(text) {
+    	$('.bubble').text(text);
+		$('.bubble').addClass('-visible'); 
+		setTimeout(function() {
+    	$('.bubble').removeClass('-visible');
+    }, 5000);
+    }
+
+	function getFirstWord() {
+		return $('.js-firstNumber').val();
+	}
+
+	function getSecondWord() {
+		return $('.js-secondNumber').val();
+	}
+
+	function checkInputData() {
+		if ($.isNumeric(getFirstWord()) && $.isNumeric(getSecondWord())) {
+			return {
+				isNumber: true
+			};
+		}else if (getFirstWord() === '' && getSecondWord() === '') {
+			return {
+				isNumber: false,
+				empty: true
+			};
+		} else {
+			return {
+				isNumber: false,
+				empty: false
+			};
+		}
+	}
 
 });
